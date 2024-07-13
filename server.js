3b13be453530d2071b2c6f19f3283424b61d1742
vino-brutor <viktorbruno2006@gmail.com>
2024-07-13 00:24:54 -0300

    server.js

--- server.js
+++ server.js
@@ -47,11 +47,6 @@ app.get('/contato', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'contato.html'));
 });
 
-// Servindo arquivos estáticos
-app.use(express.static(path.join(__dirname, 'public')));
-app.use('/node_modules/bootstrap', express.static(path.join(__dirname, 'node_modules/bootstrap/dist')));
-app.use('/node_modules/@popperjs/core', express.static(path.join(__dirname, 'node_modules/@popperjs/core/dist/umd')));
-app.use('/node_modules/swiper', express.static(path.join(__dirname, 'node_modules/swiper')));
 
 
 const PORT = process.env.PORT || 3000;
